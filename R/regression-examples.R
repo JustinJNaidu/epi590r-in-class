ead64a11f925cbf5db3dd67259b57adec3843c6f
Justin Naidu <JustinJNaidu@gmail.com>
2024-08-19 16:28:22 -0400

    worked on regression examples

--- R/regression-examples.R
+++ R/regression-examples.R
@@ -49,6 +49,11 @@ linear_model_int <- lm(income ~ sex_cat*age_bir + race_eth_cat,
 logistic_model <- glm(glasses ~ eyesight_cat + sex_cat + income,
 											data = nlsy, family = binomial())
 
+lobnomial_model
+
+poisson_model <- glm(nsibs ~ eyesight_cat + sex_cat + income,
+										 data = nlsy, family = binomial())
+
 
 ## Tables
 
@@ -95,3 +100,79 @@ tbl_int <- tbl_regression(
 
 tbl_merge(list(tbl_no_int, tbl_int),
 					tab_spanner = c("**Model 1**", "**Model 2**"))
+
+
+
+
+# In-class Exercise
+
+# Each of the univariate regression examples held the outcome (y =) constant,
+# while varying the predictor variables with include =. You can also look at one
+# predictor across several outcomes. Create a univariate regression table looking
+# at the association between sex (sex_cat) as the x = variable and each of nsibs,
+# sleep_wkdy, and sleep_wknd, and income.
+
+tbl_uvregression(
+	nlsy,
+	y = income,
+	include = c(sex_cat, sleep_wknd,
+							sleep_wkdy, income),
+	method = lm)
+
+
+
+tbl_uvregression(
+	nlsy,
+	x = sex_cat,
+	include = c(sex_cat, sleep_wknd,
+							sleep_wkdy, income),
+	method = lm)
+
+
+lm(income ~ sex_cat, data = nlsy)
+lm(nsibs ~ sex_cat, data = nlsy)
+
+# Fit a Poisson regression (family = poisson()) for the number of siblings,
+# using at least 3 predictors of your choice. Create a nice table displaying
+# your Poisson regression and its exponentiated coefficients.
+
+tbl_uvregression(
+	nlsy,
+	family = poisson(),
+	y = nsibs,
+	include = c(sex_cat, region, income),
+	method = glm)
+
+
+glm(nsibs ~ sex_cat + region + income,
+		data = nlsy,
+		family = poisson())
+exponentiate = TRUE
+
+poisson_model <- glm(nsibs ~ eyesight_cat + sex_cat + income,
+										 data = nlsy, family = binomial())
+
+tbl_no_int <- tbl_regression(
+	linear_model,
+	exponentiate = TRUE,
+	label = list(
+		sex_cat ~ "Sex",
+		eyesight_cat ~ "Eyesight",
+		income ~ "Income"
+	))
+
+
+# Instead of odds ratios for wearing glasses, as in the example, we want risk
+# ratios. We can do this by specifying in the regression family = binomial(link = "log").
+# Regress glasses on eyesight_cat sex_cat and create a table showing the risk ratios
+# and confidence intervals from this regression.
+
+# Load necessary libraries
+library(gtsummary)
+
+# Fit a binomial regression model with a log link to obtain risk ratios
+gtsummary(glm(glasses ~ eyesight_cat + sex_cat,
+											data = nlsy,
+											family = binomial(link = "log")))
+
+
